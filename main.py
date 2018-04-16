cdfdbb99c521967c5343e0ed77161b39d66e91cc
fomimatu <fomimatsu@gmail.com>
2018-04-16 17:28:04 +0900

    4/16 途中

--- main.py
+++ main.py
@@ -9,42 +9,79 @@ from selenium.webdriver.support import expected_conditions as EC
 import member as m
 
 waitSecond = 10 # 待機秒
-# ページ読取完了
 
 
-# 認証画面
-def login(driver, url):
-    driver.get(url)
+# ----------------------------------------------------------------------------------------------------------
+# 要素読み込み待ち
+# ----------------------------------------------------------------------------------------------------------
+def wait_element(web_driver, locator):
     try:
-        element = WebDriverWait(driver, waitSecond).until(
-            lambda x: x.find_element_by_tag_name("html"))
-        return driver.execute_script('return document.readyState;') == "complete"
+
+        element = WebDriverWait(web_driver, waitSecond).until(
+            # lambda x: x.find_element_by_id(elementID)
+            # EC.visibility_of_element_located((By.LINK_TEXT, name))
+            EC.visibility_of_element_located(locator)
+        )
+        return element
     except:
+        return None
+# ----------------------------------------------------------------------------------------------------------
+# Main
+# ----------------------------------------------------------------------------------------------------------
+def main():
+    options = Options()
+    # options.binary_location = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--window-size=1024,768')
+
+    """
+    # driver = webdriver.Chrome(
+        chrome_options=options, executable_path="D:\\Program\\chromedriver_win32\\chromedriver.exe")
+    """
+    driver = webdriver.Chrome(chrome_options=options)
+
+    # 認証
+    driver.get("https://recipeoffice.netcoms.ne.jp/sunco/index.html")
+    # パスワード
+
+    ele = wait_element(driver, (By.NAME, 'password'))
+    if ele == None:
         return False
+    ele.send_keys("3744")
+    # ID
+    ele = wait_element(driver, (By.NAME, 'userid'))
+    if ele == None:
+        return False
+    ele.send_keys("TOMIMATSU")
+    # 登録ボタン
+    ele = wait_element(driver, (By.ID, "login_btn"))
+    ele.click()
+
+    # 画面更新待ち
+    driver.implicitly_wait(2)  # seconds
+
+    # 管理者画面 (画面操作のみ)
+    driver.find_element_by_css_selector("a.open_user_menu").click()
+    driver.find_element_by_xpath("//a[text()='管理者設定']").click()
+    driver.find_element_by_link_text("管理者システム設定").click()
+    driver.find_element_by_link_text("利用者設定").click()
+    # 部署一覧取得
+    options = driver.find_elements_by_name("org_list")
+    for op in options:
+        txt = op.get_attribute("option")
+        val = op.get_attribute("value")
 
 
+    # スクリーンショットをとる。
+    driver.implicitly_wait(2)  # seconds
+    driver.save_screenshot('d:/tmp/pythonTest/search_results.png')
+    driver.quit()
 
 
-options = Options()
-# options.binary_location = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
-options.add_argument('--headless')
-options.add_argument('--disable-gpu')
-options.add_argument('--window-size=1024,768')
+# ----------------------------------------------------------------------------------------------------------
+# スタート
+# ----------------------------------------------------------------------------------------------------------
 
-
-
-
-# driver = webdriver.Chrome(chrome_options=options, executable_path="D:\\Program\\chromedriver_win32\\chromedriver.exe")
-driver = webdriver.Chrome(chrome_options=options)
-ele = login(driver, "https://recipeoffice.netcoms.ne.jp/sunco/index.html")
-print(ele)
-
-# driver.get("http://tokyo-ame.jwa.or.jp/")
-
-# スクリーンショットをとる。
-driver.save_screenshot('d:/tmp/pythonTest/search_results.png')
-driver.quit()
-
-
-
+main()
 
