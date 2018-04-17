81c8dc4143849087e23b82a7be4851bc8eac37af
fomimatu <fomimatsu@gmail.com>
2018-04-17 17:26:23 +0900

    4/17 まだ途中

--- main.py
+++ main.py
@@ -8,7 +8,20 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import member as m
 
-waitSecond = 10 # 待機秒
+waitSecond = 5  # 待機秒
+
+
+# ----------------------------------------------------------------------------------------------------------
+# ページ読み込み待ち
+# ----------------------------------------------------------------------------------------------------------
+def page_loaded(web_driver):
+   try:
+        WebDriverWait(web_driver, waitSecond).until(
+            lambda x: x.execute_script('return document.readyState;') == 'complete'
+        )
+        return True
+   except:
+        return False
 
 
 # ----------------------------------------------------------------------------------------------------------
@@ -25,6 +38,8 @@ def wait_element(web_driver, locator):
         return element
     except:
         return None
+
+
 # ----------------------------------------------------------------------------------------------------------
 # Main
 # ----------------------------------------------------------------------------------------------------------
@@ -40,22 +55,30 @@ def main():
         chrome_options=options, executable_path="D:\\Program\\chromedriver_win32\\chromedriver.exe")
     """
     driver = webdriver.Chrome(chrome_options=options)
+    # タイムアウトまでの時間
+    driver.set_page_load_timeout(waitSecond)
 
     # 認証
+    # try:  # タイムアウト例外が起きる
     driver.get("https://recipeoffice.netcoms.ne.jp/sunco/index.html")
-    # パスワード
+    if not page_loaded(driver):
+        raise Exception
 
-    ele = wait_element(driver, (By.NAME, 'password'))
+    # パスワード
+    locator = (By.NAME, 'password')
+    ele = wait_element(driver, locator)
     if ele == None:
         return False
     ele.send_keys("3744")
     # ID
-    ele = wait_element(driver, (By.NAME, 'userid'))
+    locator = (By.NAME, 'userid')
+    ele = wait_element(driver, locator)
     if ele == None:
         return False
     ele.send_keys("TOMIMATSU")
     # 登録ボタン
-    ele = wait_element(driver, (By.ID, "login_btn"))
+    locator = (By.ID, "login_btn")
+    ele = wait_element(driver, locator)
     ele.click()
 
     # 画面更新待ち
@@ -67,16 +90,46 @@ def main():
     driver.find_element_by_link_text("管理者システム設定").click()
     driver.find_element_by_link_text("利用者設定").click()
     # 部署一覧取得
-    options = driver.find_elements_by_name("org_list")
-    for op in options:
-        txt = op.get_attribute("option")
-        val = op.get_attribute("value")
+    options = driver.find_elements_by_xpath("//select[@name='org_list']/option")
+    idx_list = list(map(lambda x: x.get_attribute("value"), options))
+    busho_list = list(map(lambda x: x.text.strip(), options))
+    dic_busho = dict(zip(idx_list, busho_list))
+    member_list = []
+    # 名前取得
 
+    for key,val in dic_busho.items():
+        # 部署クリック
+        locator = (By.XPATH, "//select[@name='org_list']/option[@value='" + key + "']")
+        ele = wait_element(driver, locator)
+        if ele == None:
+            return False
+        ele.click()
+        driver.implicitly_wait(1)  # seconds
+
+        locator = (By.XPATH, "//table[@class='layout']/tbody/tr/td/ul/li/a")
+        if wait_element(driver, locator) != None:
+            tags = driver.find_elements_by_xpath(locator[1])
+            members = list(map(lambda x: x.text.replace("◎", "").strip(), tags))
+            member_list.append({val : members})
+        print(len(member_list))
+
+
+
+
+
+
+
+
+    # ここから先は、必ず一度は固まるがこのまま作る
 
     # スクリーンショットをとる。
     driver.implicitly_wait(2)  # seconds
     driver.save_screenshot('d:/tmp/pythonTest/search_results.png')
+# except:
+    # return False
+# finally:
     driver.quit()
+
 
 
 # ----------------------------------------------------------------------------------------------------------
--- member.py
+++ member.py
@@ -5,36 +5,11 @@
 """
 class MemberData:
     def __init__(self):
-        self.shainCD = None # 社員番号
-        self.shainNM = None # 名前
-        self.kana = None # かな
-        self.naisen = None # 内線
-        self.userID = None # ユーザＩＤ
-        self.mail = None # メールアドレス
-        self.section = None # 部署
-        self.post = None # 役職
-
-
-"""
-DVD情報を格納
-"""
-class DvdInfo:
-    def __init__(self, searchCode):
-        self.searchCode = searchCode  # 検索ワードより、パターンマッチ(英字3-5文字 & "-" & 数字3-5文字)により抽出
-        self.hinban = None  # 検索サイトでの品番（searchCodeの方が一般的になる）
-        self.title = None   # タイトル
-        self.director = None    # 監督
-        self.series = None    # シリーズ
-        self.releaseDate = None    # 発売日
-        self.maker = None    # メーカー
-        self.label = None    # レーベル
-        self.performer = None    # 出演者
-        self.junle = None    # ジャンル
-        self.recTime = None    # 収録時間
-        self.comment = None    # コメント
-        self.shosaiUrl = None   # 詳細ページのURL
-        self.imageL = None    # 画像L
-        self.imageS = None    # 画像S
-        self.mode = None    # DVDの種類
-        self.searchTime = None  # 検索時間
-        self.searchSite = None  # 検索サイト
+        self.shainCD = None  # 社員番号
+        self.shainNM = None  # 名前
+        self.kana = None  # かな
+        self.naisen = None  # 内線
+        self.userID = None  # ユーザＩＤ
+        self.mail = None  # メールアドレス
+        self.section = None  # 部署
+        self.post = None  # 役職
