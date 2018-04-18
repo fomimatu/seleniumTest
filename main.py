e71ad9a79006e77be3074bd0533f39f897f90c5c
fomimatu <fomimatsu@gmail.com>
2018-04-18 20:23:58 +0900

    20018/04/18

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import time
+import sys
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
@@ -8,7 +9,9 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import member as m
 
-waitSecond = 5  # 待機秒
+waitSecond = 3  # 待機秒 (デバッグモードの時は長めにする）
+waitRetry = 3
+
 
 
 # ----------------------------------------------------------------------------------------------------------
@@ -27,39 +30,61 @@ def page_loaded(web_driver):
 # ----------------------------------------------------------------------------------------------------------
 # 要素読み込み待ち
 # ----------------------------------------------------------------------------------------------------------
+def wait_element2(web_driver, locator):
+    element = WebDriverWait(web_driver, waitSecond).until(
+        # lambda x: x.find_element_by_id(elementID)
+        # EC.presence_of_element_located(locator)
+        EC.visibility_of_element_located(locator)
+    )
+    return element
+
+# ----------------------------------------------------------------------------------------------------------
+# 要素読み込み待ち
+# ----------------------------------------------------------------------------------------------------------
+def element_wait(web_driver, locator):
+    element = WebDriverWait(web_driver, waitSecond).until(
+        # lambda x: x.find_element_by_id(elementID)
+        # EC.presence_of_element_located(locator)
+        EC.visibility_of_element_located(locator)
+    )
+    return element
+# ----------------------------------------------------------------------------------------------------------
+# 要素読み込み待ち
+# ----------------------------------------------------------------------------------------------------------
 def wait_element(web_driver, locator):
-    try:
-
-        element = WebDriverWait(web_driver, waitSecond).until(
-            # lambda x: x.find_element_by_id(elementID)
-            # EC.visibility_of_element_located((By.LINK_TEXT, name))
-            EC.visibility_of_element_located(locator)
-        )
-        return element
-    except:
-        return None
-
+    ele = None
+    i = 0
+    while (ele == None):
+        try:
+            ele = element_wait(web_driver, locator)
+        except Exception as ex:
+            web_driver.implicitly_wait(1)
+            print(type(ex))
+            if i > waitRetry:
+                return None
+            i += 1
+    return ele
 
 # ----------------------------------------------------------------------------------------------------------
-# Main
+# webDriver 初期化
 # ----------------------------------------------------------------------------------------------------------
-def main():
+def driver_init(driver):
     options = Options()
     # options.binary_location = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')
     options.add_argument('--window-size=1024,768')
-
     """
     # driver = webdriver.Chrome(
         chrome_options=options, executable_path="D:\\Program\\chromedriver_win32\\chromedriver.exe")
     """
     driver = webdriver.Chrome(chrome_options=options)
-    # タイムアウトまでの時間
-    driver.set_page_load_timeout(waitSecond)
+    return driver
 
-    # 認証
-    # try:  # タイムアウト例外が起きる
+# ----------------------------------------------------------------------------------------------------------
+# ログオンから利用者設定画面まで（ここまでは以下の機能共通）
+# ----------------------------------------------------------------------------------------------------------
+def logon_to_kairi(driver):
     driver.get("https://recipeoffice.netcoms.ne.jp/sunco/index.html")
     if not page_loaded(driver):
         raise Exception
@@ -70,6 +95,7 @@ def main():
     if ele == None:
         return False
     ele.send_keys("3744")
+
     # ID
     locator = (By.NAME, 'userid')
     ele = wait_element(driver, locator)
@@ -89,45 +115,71 @@ def main():
     driver.find_element_by_xpath("//a[text()='管理者設定']").click()
     driver.find_element_by_link_text("管理者システム設定").click()
     driver.find_element_by_link_text("利用者設定").click()
-    # 部署一覧取得
+
+
+# ----------------------------------------------------------------------------------------------------------
+# 部署一覧取得
+# <param>   webDriver
+# <return>  Dictionary{ IDX : 部署名 }
+# ----------------------------------------------------------------------------------------------------------
+def busho_get(driver):
     options = driver.find_elements_by_xpath("//select[@name='org_list']/option")
     idx_list = list(map(lambda x: x.get_attribute("value"), options))
     busho_list = list(map(lambda x: x.text.strip(), options))
-    dic_busho = dict(zip(idx_list, busho_list))
+    return list(zip(idx_list, busho_list))
+
+
+# ----------------------------------------------------------------------------------------------------------
+# Main
+# ----------------------------------------------------------------------------------------------------------
+def main():
+    driver = None
+    driver = driver_init(driver)
+
+    # 認証
+    logon_to_kairi(driver)
+    # 部署一覧取得
+    tpl_busho = busho_get(driver)
+
+    # sys.exit()  # 途中終了
+
+    # 部署から名前一覧取得
+    # ここから巡回中落ちる場合がある
     member_list = []
-    # 名前取得
+    remove_keys = []
+    while len(tpl_busho) != 0:
+        print("start", len(tpl_busho))
+        for key, val in tpl_busho:
+            try:
+                # 部署クリック
+                locator = (By.XPATH, "//select[@name='org_list']/option[@value='" + key + "']")
+                ele = wait_element(driver, locator)
+                if ele == None:
+                    raise Exception("None error!")
+                driver.implicitly_wait(1)  # seconds
+                ele.click()
+                print(val)
+                """
+                locator = (By.XPATH, "//table[@class='layout']/tbody/tr/td/ul/li/a")
+                if wait_element(driver, locator) != None:
+                    tags = driver.find_elements_by_xpath(locator[1])
+                    members = list(map(lambda x: x.text.replace("◎", "").strip(), tags))
+                    member_list.append({val : members})
+                print(val, len(member_list))
+                """
+                remove_keys.append(key)
+            except Exception as e:
+                print(type(e))
 
-    for key,val in dic_busho.items():
-        # 部署クリック
-        locator = (By.XPATH, "//select[@name='org_list']/option[@value='" + key + "']")
-        ele = wait_element(driver, locator)
-        if ele == None:
-            return False
-        ele.click()
-        driver.implicitly_wait(1)  # seconds
-
-        locator = (By.XPATH, "//table[@class='layout']/tbody/tr/td/ul/li/a")
-        if wait_element(driver, locator) != None:
-            tags = driver.find_elements_by_xpath(locator[1])
-            members = list(map(lambda x: x.text.replace("◎", "").strip(), tags))
-            member_list.append({val : members})
-        print(len(member_list))
-
-
-
-
-
-
-
+        tpl_busho = list(filter(lambda x: x[0] not in remove_keys, tpl_busho))
+        print("end", len(tpl_busho))
 
     # ここから先は、必ず一度は固まるがこのまま作る
 
     # スクリーンショットをとる。
     driver.implicitly_wait(2)  # seconds
     driver.save_screenshot('d:/tmp/pythonTest/search_results.png')
-# except:
-    # return False
-# finally:
+
     driver.quit()
 
 
@@ -135,6 +187,6 @@ def main():
 # ----------------------------------------------------------------------------------------------------------
 # スタート
 # ----------------------------------------------------------------------------------------------------------
-
-main()
+if __name__ == "__main__":
+    main()
 
